60bf5258cb99cf7f49521aa00e46dd794d16157d
juanArguello <juanarguello092@gmail.com>
2023-03-03 20:57:22 -0300

    Creacion de proyecto spring backend

---
+++ src/main/java/com/sgse/SgseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sgse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgseBackendApplication.class, args);
+	}
+
+}
